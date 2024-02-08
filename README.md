8a3bcfe0b413d80260ee6c894231baaf3dc44866
Fernando Cordero <fernando.corderobastidas@cencosud.cl>
2024-02-08 18:07:22 -0300

    ADD: Methods and functions

---
+++ README.md
@@ -0,0 +1,22 @@
+## Receiver function
+
+```go
+  type Employee struct {
+	ID        int
+	Name      string
+	Address   string
+	DoB       time.Time
+	Position  string
+	Salary    int
+	ManagerID int
+}
+
+func (e *Employee) SetId(id int) {
+	e.ID = id
+}
+
+func (e *Employee) GetId() int {
+	return e.ID
+}
+
+```
--- main.go
+++ main.go
@@ -5,28 +5,70 @@ import (
 	"time"
 )
 
+type Employee struct {
+	ID        int
+	Name      string
+	Address   string
+	DoB       time.Time
+	Position  string
+	Salary    int
+	ManagerID int
+}
+
+func (e *Employee) SetId(id int) {
+	e.ID = id
+}
+
+func (e *Employee) GetId() int {
+	return e.ID
+}
+
+func (e *Employee) SetName(name string) {
+	e.Name = name
+}
+
+func (e *Employee) GetName() string {
+	return e.Name
+}
+
 func main() {
-	m := make(map[string]int)
-	m["a"] = 1
-	fmt.Println(m["a"])
-
-	s := []int{1, 2, 3}
-	s = append(s, 8)
-	for index, value := range s {
-		fmt.Println(index, value)
+	e := Employee{
+		ID:        1,
+		Name:      "John",
+		Address:   "123 Main St",
+		DoB:       time.Now(),
+		Position:  "Developer",
+		Salary:    50000,
+		ManagerID: 1,
 	}
-	c := make(chan int)
-	go doSomething(c)
-	<-c
-	g := 25
-	fmt.Println(g)
-	h := &g
-	fmt.Println(h)
-	fmt.Println(*h)
+	e.SetId(555)
+	e.SetName("Jane555")
+	fmt.Println(e.GetName())
+	fmt.Println(e.GetId())
 }
 
-func doSomething(c chan int) {
-	time.Sleep(3 * time.Second)
-	fmt.Println("Done")
-	c <- 1
-}
+// func main() {
+// 	m := make(map[string]int)
+// 	m["a"] = 1
+// 	fmt.Println(m["a"])
+
+// 	s := []int{1, 2, 3}
+// 	s = append(s, 8)
+// 	for index, value := range s {
+// 		fmt.Println(index, value)
+// 	}
+// 	c := make(chan int)
+// 	go doSomething(c)
+// 	<-c
+// 	g := 25
+// 	fmt.Println(g)
+// 	h := &g
+// 	fmt.Println(h)
+// 	fmt.Println(*h)
+// }
+
+// func doSomething(c chan int) {
+// 	time.Sleep(3 * time.Second)
+// 	fmt.Println("Done")
+// 	c <- 1
+// }
