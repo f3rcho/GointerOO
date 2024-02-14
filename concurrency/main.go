5327abc4c16dc000902ab580c0017a052f4a1cbb
Fernando Cordero <fernando.corderobastidas@cencosud.cl>
2024-02-14 10:48:30 -0300

    ADD: buffered and unbuffered

---
+++ concurrency/main.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+// import "time"
+
+// func ProduceNumbers(ch chan<- int) {
+// 	for i := 0; i < 10; i++ {
+// 		ch <- i
+// 		ch <- i
+// 	}
+// 	close(ch)
+// }
+
+// func ConsumeNumbers(ch <-chan int) {
+// 	for num := range ch {
+// 		println(num)
+// 	}
+// }
+
+// func main() {
+// 	ch := make(chan int) // unbuffered channel
+// 	go ProduceNumbers(ch)
+// 	time.Sleep(5 * time.Second)
+// 	ConsumeNumbers(ch)
+// }
+
+func main() {
+	ch := make(chan string, 3) // Create a buffered channel with capacity 3
+
+	go func() {
+		ch <- "message 1"
+		ch <- "message 2"
+		ch <- "message 3"
+		close(ch)
+	}()
+
+	for msg := range ch {
+		fmt.Println("Received:", msg)
+	}
+}
